00c7ebaf715e8b8289c6994e8701b12bd1b24e3a
Yule <paul@dryule.com>
2011-08-11 13:59:14 +0100

    better binary

--- lib/haddock.rb
+++ lib/haddock.rb
@@ -1,7 +1,7 @@
 # A more memorable password generator. Swordfish? No, I got tired of that. I
 # changed it.
 module Haddock
-  VERSION = '0.2.5'
+  VERSION = '0.2.7'
 
   module Password
     # The minimum password legnth.
